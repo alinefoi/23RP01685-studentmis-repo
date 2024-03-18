b1e39f65d3bf6e847032e625d605a0ee77ff1d6e
unknown <alinefoi24@gmail.com>
2024-03-18 15:34:37 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   echo "Welcome to Students MIS";
+?> 
+<a href="">registration page</a>
+</body>
+</html>
