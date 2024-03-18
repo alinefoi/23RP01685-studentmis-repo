9380bcc0e32734eba1b5bfa5f46eb7424f81723d
alinefoi <77851844+alinefoi@users.noreply.github.com>
2024-03-18 17:12:39 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,152 +2,16 @@
 ?> 
 <!DOCTYPE html> <html> <head>  <title> USER REGISTRATION</title> 
 <body>
-<style>
-			.y{
-		height:27px;
-		border-radius: 5px;
-		color:blue;
-		background:#efe7eb;
-		font-size: 23px;
-		font-family:Juice ITC;
-		width: 300px;
-		border: none;
-		
-		text-align: center;
-		box-shadow:  3px 2px blue;
-     
-		
-		}
-	.y:hover{
-			background:#ffffff;
-			color:  blue;
-			font-size: 20px;
-			font-family:Monotype Corsiva;
-			}
-                       background{
-		height:27px;
 
-		width: 148px;
-		border: none;
-		
-		text-align: center;
-		
-		
-			}
-			#p{
-		height:27px;
-		border-radius: 0px 20px;
-		color:white;
-		background:blue;
-		font-size: 23px;
-		font-family:Juice ITC;
-		width: 148px;
-		border: none;
-		
-		text-align: center;
-		box-shadow:  3px 4px black;
-		
-			}
-			#p:hover{
-			background:black;
-			color:white;
-			font-size: 20px;
-			font-family:Juice ITC;	
-			}
-                        
-                        #btn{
-		height:32px;
-		border-radius:5px;
-                
-		color:white;
-		background:#007bff;
-                margin-top: -22px;
-		width: 118px;
-		border: none;
-                font-family: Arial Black; 
-                font-size: 20px;
-		margin-right:-970px; 
-		text-align: center;
-		box-shadow:  3px 3px blue;
-		
-			}
-			#btn:hover{
-			background:blue;
-			color:white;
-			font-size: 20px;
-			font-family:Juice ITC;	
-			}
-                        #logout{
-		height:35px;
-		border-radius:5px;
-		color:white;
-		background:#007bff;
-     
-		width: 428px;
-		border: none;
-                font-family: Arial Black; 
-                font-size: 20px;
-		
-		text-align: center;
-		box-shadow:  1px 2px white;
-                margin-top: 35px;
-		
-			}
-			#logout:hover{
-			background:#007bff;
-			color:white;
-			font-size: 20px;
-			font-family:Juice ITC;	
-			}
-                         #fab{
-                         height:32px;
-		border-radius: 9px;
-		color:  blue;
-		background:white;
-		font-size: 23px;
-		font-family:Juice ITC;
-		width: 260px;
-		border: none;
-               
-                
-                margin-top: -20px;
-		
-		text-align: center;
-		box-shadow: 2px 6px 10px blue;
-        
-                        }
-                        #fab:hover{
-                        border-radius: 20px;
-                        background:#ffffff;
-			color:  blue;
-			font-size: 20px;
-			font-family:Monotype Corsiva;
-                     }
-              
-					 .mnu
-{
-background-color:#000000;
-padding:4px 7px 4px 7px;
-color:#FFFFFF;
-text-decoration:none;
--moz-border-radius:3px 3px 0 0;
--webkit-border-radius:3px 3px 0 0;
-border-radius:3px 3px 0 0;
-text-transform:uppercase;
-
-}         
-
-
-		</style>
         <div>
 
 </head>
  <body><center>   <form action="" method="POST"> 
-Names<input type="text" name="name"  class="y"> </b>
+Names<input type="text" name="name"  class="y"> </br></br>
 
 
-Email<input type="email" name="email" class="y"></b>
-password<input type="password" name="pass" class="y"> </b>
+Email<input type="email" name="email" class="y"></br></br>
+password<input type="password" name="pass" class="y"> </br></br>
 
 <input type="submit" name="submit" id="p"> 
 </div>
